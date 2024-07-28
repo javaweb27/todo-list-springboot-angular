dcac1da212977320125798420b9a9fea132735f6
javier <devjavicont@gmail.com>
2024-07-28 17:23:33 -0400

    init git repo

---
+++ todo_list_server/src/main/java/javier/todo_list_server/TodoListServerApplication.java
@@ -0,0 +1,13 @@
+package javier.todo_list_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListServerApplication.class, args);
+	}
+
+}
---
+++ todo_list_server/src/test/java/javier/todo_list_server/TodoListServerApplicationTests.java
@@ -0,0 +1,13 @@
+package javier.todo_list_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
